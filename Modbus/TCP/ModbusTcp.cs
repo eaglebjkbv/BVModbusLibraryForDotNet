1f1b7900ea5e3c67974e2437d75e763f976ae172
unknown <bbvardal@gmail.com>
2022-02-04 17:01:06 +0300

    Write multiple register command added

--- Modbus/TCP/ModbusTcp.cs
+++ Modbus/TCP/ModbusTcp.cs
@@ -28,16 +28,8 @@ namespace BV.Modbus.TCP
         private NetworkStream _networkStream;
         public string _ip { get; set; }
         public int _port { get; set; }
-        private int _unitAddress = 0;
-        private Byte[] _data ={
-                0x00,0x01,
-                0x00,0x00,
-                0x00,0x06,
-                0x11,
-                0x03,
-                0x00,0x00,
-                0x00,0x01
-        };
+        private byte _unitAddress = 0;
+        private Byte[] _data=new Byte[512]; 
         private Byte[] _rData = new Byte[512];
         
         /// <summary>
@@ -47,8 +39,17 @@ namespace BV.Modbus.TCP
         /// <param name="port">Port number of Modbus TCP server Default value is 512</param>
         /// <param name="unitAddress">Unit Number of Modbus Serial unit behind Modbus TCP Server Gateway. Default value is 1</param>
 
-        public ModbusTcp(string ip,int port=502,int unitAddress = 1)
+        public ModbusTcp(string ip,int port=502,byte unitAddress = 1)
         {
+            _data[0] = 0x00;_data[1] = 0x01;
+            _data[2] = 0x00;_data[3] = 0x00;
+            _data[4] = 0x00; _data[5] = 0x06;
+            _data[6] = 0x011;
+            _data[7] = 0x03;
+            _data[8] = 0x00;_data[9] = 0x00;
+            _data[10] = 0x00;_data[11] = 0x01;
+             
+      
             _ip = ip;
             _port = port;
             _unitAddress = unitAddress;
@@ -94,22 +95,82 @@ namespace BV.Modbus.TCP
                 return false;
             }
         }
-        public bool SendCommand(ModbusCmd cmd,UInt16 regNr,UInt16 value)
+        public bool WriteMultipleRegisters(uint regNr,List<UInt16> values)
+        {
+            bool result = false;
+            try
+            {
+                int len = values.Count;
+                Console.WriteLine($"uzunluk:{len.ToString()}");
+                if (len * 2 > 255) new Exception("Cannot be more than 255 bytes");
+                
+                
+
+                //byte[] valBytes = BitConverter.GetBytes(value);
+                if (_networkStream != null){
+                    byte[] lenFrame = BitConverter.GetBytes((len * 2) + 7);
+                    byte[] lenByte = BitConverter.GetBytes(len*2);
+                    byte[] lenCount = BitConverter.GetBytes(len);
+                    byte[] regBytes = BitConverter.GetBytes(regNr);
+
+                    _data[4] = lenFrame[1]; _data[5] = lenFrame[0];
+                    _data[6] = _unitAddress;
+                    _data[7] = 0x10;
+                    _data[8] = regBytes[1];_data[9] = regBytes[0];
+                    _data[10] = lenCount[1]; _data[11] = lenCount[0];
+                    _data[12] = lenByte[0];
+                    
+
+                    int i = 0;
+                    foreach (var value in values)
+                    {
+                        byte[] valBytes = BitConverter.GetBytes(value);
+                        _data[13 + i] = valBytes[1];
+                        _data[14 + i] = valBytes[0];
+                        //Console.Write($"-{_data[13 + i].ToString()}");
+                        //Console.Write($"-{_data[14 + i].ToString()}");
+                        i = i + 2 ;
+                    }
+                    _networkStream.Write(_data, 0, 13+len*2);
+
+                    while (true)
+                    {
+                        int bytes = _networkStream.Read(_rData, 0, 12);
+                        if (bytes > 0)
+                        {
+                            //foreach (byte d in _rData)
+                            //    Console.Write($"-{d.ToString("X2")}");
+                            if (_rData[7] == 0x10) result = true;
+                            break;
+
+                        }
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return result;
+        }
+
+
+
+        public bool WriteSingleRegister(UInt16 regNr,UInt16 value)
         {
             bool result = false;
             try
             {
                 byte[] regBytes = BitConverter.GetBytes(regNr);
                 byte[] valBytes = BitConverter.GetBytes(value);
-                byte[] unitBytes = BitConverter.GetBytes(_unitAddress);
+                
 
                 if (_networkStream != null)
                 {
-                    _data[6] = unitBytes[0];
-
-                    if(cmd==ModbusCmd.writeSingleRegister_FC6) _data[7] = 0x06; // Write Single Register command
-                    if(cmd == ModbusCmd.readRegisters_FC3) _data[7] = 0x03; // Read registers
-                    
+                    _data[6] = _unitAddress;
+                    _data[7] = 0x06;
                     _data[8] = regBytes[1];
                     _data[9] = regBytes[0];
                     _data[10] = valBytes[1];
@@ -117,10 +178,10 @@ namespace BV.Modbus.TCP
                     //foreach (byte d in _data)
                     //    Console.Write($"-{d.ToString("X2")}");
                     //Console.WriteLine();
-                    _networkStream.Write(_data, 0, _data.Length);
+                    _networkStream.Write(_data, 0, 12);
                     while (true)
                     {
-                        int bytes = _networkStream.Read(_rData, 0, _rData.Length);
+                        int bytes = _networkStream.Read(_rData, 0, 12);
                         if (bytes > 0)
                         {
                             //foreach (byte d in _rData)
--- Test/Program.cs
+++ Test/Program.cs
@@ -16,9 +16,21 @@ namespace Test
             if (mbTcp.IsReady())
             {
                 Console.WriteLine("Modbus Bağlantı hazır....");
-                mbTcp.SendCommand(ModbusCmd.writeSingleRegister_FC6, 0x0000, 0x00FF);
+
+                bool result = mbTcp.WriteMultipleRegisters(0x0000, new List<ushort> { 0xABCD, 0x01111,0x2222,0x4444 });
+
+                // bool result=mbTcp.WriteSingleRegister(0x0000,0x00FF);
+                if (result)
+                {
+                    Console.WriteLine("Başarılı");
+                }
+                else
+                {
+                    Console.WriteLine("Başarısız !!!");
+                }
                 mbTcp.DisConnect();
             }
+            Console.ReadLine();
         }
     }
 }
